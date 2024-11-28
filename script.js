c2eab7c781ca5646cd4aa8f4f9b679fbea00cb91
mikaele2222 <mfs52@aluno.ifal.edu.br>
2024-11-28 17:49:38 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let numero = parseFloat(prompt('digite um numero: '));
+if (numero < 0) {
+    alert("voce digitou um numero negativo");
+  
+} else if(numero > 0){
+
+    alert("voce digitou um numero positivo");
+
+} else {
+    
+    alert("voce digitou zero");
+}
+
